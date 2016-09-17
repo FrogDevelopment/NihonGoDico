d382681de61e853710c7e278fab1dc0d28f584b0
Le Gall Benoit <legall.benoit@gmail.com>
2016-09-17 18:43:51 +0200

    search by kanji only, or by kana if no kanji

--- app/src/main/java/fr/frogdevelopment/nihongo/dico/DetailsActivity.java
+++ app/src/main/java/fr/frogdevelopment/nihongo/dico/DetailsActivity.java
@@ -33,156 +33,153 @@ import fr.frogdevelopment.nihongo.dico.entities.Example;
 
 public class DetailsActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    @BindView(R.id.details_kanji)
-    TextView mKanji;
+	@BindView(R.id.details_kanji)
+	TextView mKanji;
 
-    @BindView(R.id.details_reading)
-    TextView mReading;
+	@BindView(R.id.details_reading)
+	TextView mReading;
 
-    @BindView(R.id.details_lexicon)
-    TextView mLexicon;
+	@BindView(R.id.details_lexicon)
+	TextView mLexicon;
 
-    @BindView(R.id.details_gloss)
-    TextView mGloss;
+	@BindView(R.id.details_gloss)
+	TextView mGloss;
 
-    @BindView(R.id.details_info)
-    TextView mInfo;
+	@BindView(R.id.details_info)
+	TextView mInfo;
 
-    @BindView(R.id.details_show_examples)
-    Button mButtonExamples;
+	@BindView(R.id.details_show_examples)
+	Button mButtonExamples;
 
-    @BindView(R.id.details_examples)
-    ListView mExamples;
+	@BindView(R.id.details_examples)
+	ListView mExamples;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_details);
 
-        ButterKnife.bind(this);
+		ButterKnife.bind(this);
 
-        setActionBar(ButterKnife.findById(this, R.id.toolbar));
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+		setActionBar(ButterKnife.findById(this, R.id.toolbar));
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        Bundle args = getIntent().getExtras();
+		Bundle args = getIntent().getExtras();
 
-        mKanji.setText(args.getString(EntryContract.KANJI));
-        mReading.setText(args.getString(EntryContract.READING));
+		mKanji.setText(args.getString(EntryContract.KANJI));
+		mReading.setText(args.getString(EntryContract.READING));
 
-        getLoaderManager().initLoader(1, args, this);
-    }
+		getLoaderManager().initLoader(1, args, this);
+	}
 
 //    @OnClick(R.id.fab)
 //    void onFavoriteFabClick(View view) {
 //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
 //    }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        if (id == 1) {
-            String[] columns = {SenseContract.POS, SenseContract.FIELD, SenseContract.MISC, SenseContract.DIAL, SenseContract.GLOSS, SenseContract.INFO};
-            String selection = SenseContract._ID + "=" + args.getLong(SenseContract._ID);
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		if (id == 1) {
+			String[] columns = {SenseContract.POS, SenseContract.FIELD, SenseContract.MISC, SenseContract.DIAL, SenseContract.GLOSS, SenseContract.INFO};
+			String selection = SenseContract._ID + "=" + args.getLong(SenseContract._ID);
 
-            return new CursorLoader(this, NihonGoDicoContentProvider.URI_WORD, columns, selection, null, null);
-        } else if (id == 2) {
-            String[] columns = {ExampleContract.JAPANESE_SENTENCE, ExampleContract.TRANSLATION_SENTENCE};
-            List<String> tmp = new ArrayList<>();
-            String kanji = mKanji.getText().toString();
-            if (StringUtils.isNotBlank(kanji)) {
-                tmp.add(kanji);
-            }
-            String reading = mReading.getText().toString();
-            if (StringUtils.isNotBlank(reading)) {
-                tmp.add(reading);
-            }
-            String[] selectionArgs = {TextUtils.join(" OR ", tmp)};
+			return new CursorLoader(this, NihonGoDicoContentProvider.URI_WORD, columns, selection, null, null);
+		} else if (id == 2) {
+			String[] columns = {ExampleContract.JAPANESE_SENTENCE, ExampleContract.TRANSLATION_SENTENCE};
+			String[] selectionArgs;
+			String kanji = mKanji.getText().toString();
+			if (StringUtils.isNotBlank(kanji)) {
+				selectionArgs = new String[]{kanji};
+			} else {
+				selectionArgs = new String[]{mReading.getText().toString()};
+			}
 
-            return new CursorLoader(this, NihonGoDicoContentProvider.URI_EXAMPLE, columns, null, selectionArgs, null);
-        }
+			return new CursorLoader(this, NihonGoDicoContentProvider.URI_EXAMPLE, columns, null, selectionArgs, null);
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        if (loader.getId() == 1) {
-            Details item = new Details();
-            if (data.moveToNext()) {
-                item.pos = data.getString(0);
-                item.field = data.getString(1);
-                item.misc = data.getString(2);
-                item.dial = data.getString(3);
-                item.gloss = data.getString(4);
-                item.info = data.getString(5);
-            }
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		if (loader.getId() == 1) {
+			Details item = new Details();
+			if (data.moveToNext()) {
+				item.pos = data.getString(0);
+				item.field = data.getString(1);
+				item.misc = data.getString(2);
+				item.dial = data.getString(3);
+				item.gloss = data.getString(4);
+				item.info = data.getString(5);
+			}
 
-            List<String> lexicon = new ArrayList<>();
-            if (StringUtils.isNotBlank(item.pos)) {
-                lexicon.add(item.pos);
-            }
-            if (StringUtils.isNotBlank(item.field)) {
-                lexicon.add(item.field);
-            }
-            if (StringUtils.isNotBlank(item.misc)) {
-                lexicon.add(item.misc);
-            }
-            if (StringUtils.isNotBlank(item.dial)) {
-                lexicon.add(item.dial);
-            }
+			List<String> lexicon = new ArrayList<>();
+			if (StringUtils.isNotBlank(item.pos)) {
+				lexicon.add(item.pos);
+			}
+			if (StringUtils.isNotBlank(item.field)) {
+				lexicon.add(item.field);
+			}
+			if (StringUtils.isNotBlank(item.misc)) {
+				lexicon.add(item.misc);
+			}
+			if (StringUtils.isNotBlank(item.dial)) {
+				lexicon.add(item.dial);
+			}
 
-            if (lexicon.isEmpty()) {
-                mLexicon.setText("");
-                mLexicon.setVisibility(View.GONE);
-            } else {
-                mLexicon.setText(TextUtils.join(" / ", lexicon)); // fixme rendre clickable => afficher liste lexique
-                mLexicon.setVisibility(View.VISIBLE);
-            }
+			if (lexicon.isEmpty()) {
+				mLexicon.setText("");
+				mLexicon.setVisibility(View.GONE);
+			} else {
+				mLexicon.setText(TextUtils.join(" / ", lexicon)); // fixme rendre clickable => afficher liste lexique
+				mLexicon.setVisibility(View.VISIBLE);
+			}
 
-            mInfo.setText(item.info);
-            if (StringUtils.isBlank(item.info)) {
-                mInfo.setVisibility(View.GONE);
-            } else {
-                mInfo.setVisibility(View.VISIBLE);
-            }
+			mInfo.setText(item.info);
+			if (StringUtils.isBlank(item.info)) {
+				mInfo.setVisibility(View.GONE);
+			} else {
+				mInfo.setVisibility(View.VISIBLE);
+			}
 
-            mGloss.setText(item.gloss);
-        } else if (loader.getId() == 2) {
-            List<Example> examples = new ArrayList<>();
+			mGloss.setText(item.gloss);
+		} else if (loader.getId() == 2) {
+			List<Example> examples = new ArrayList<>();
 
-            Pattern patternKanji = Pattern.compile(mKanji.getText().toString());
-            Pattern patternReading = Pattern.compile(mReading.getText().toString());
-            Matcher matcher;
-            while (data.moveToNext()) {
-                String japanese = data.getString(0);
-                Example example = new Example(japanese, data.getString(1));
-                matcher = patternKanji.matcher(japanese);
-                while (matcher.find()) {
-                    example.matchIndices.add(Pair.of(matcher.start(), matcher.end()));
-                }
-                matcher = patternReading.matcher(japanese);
-                while (matcher.find()) {
-                    example.matchIndices.add(Pair.of(matcher.start(), matcher.end()));
-                }
-                examples.add(example);
-            }
+			Pattern patternKanji = Pattern.compile(mKanji.getText().toString());
+			Pattern patternReading = Pattern.compile(mReading.getText().toString());
+			Matcher matcher;
+			while (data.moveToNext()) {
+				String japanese = data.getString(0);
+				Example example = new Example(japanese, data.getString(1));
+				matcher = patternKanji.matcher(japanese);
+				while (matcher.find()) {
+					example.matchIndices.add(Pair.of(matcher.start(), matcher.end()));
+				}
+				matcher = patternReading.matcher(japanese);
+				while (matcher.find()) {
+					example.matchIndices.add(Pair.of(matcher.start(), matcher.end()));
+				}
+				examples.add(example);
+			}
 
-            mButtonExamples.setVisibility(View.INVISIBLE);
-            mExamples.setVisibility(View.VISIBLE);
-            mExamples.setAdapter(new ExampleAdapter(this, examples));
-        }
+			mButtonExamples.setVisibility(View.INVISIBLE);
+			mExamples.setVisibility(View.VISIBLE);
+			mExamples.setAdapter(new ExampleAdapter(this, examples));
+		}
 
-        data.close();
-        getLoaderManager().destroyLoader(loader.getId());
-    }
+		data.close();
+		getLoaderManager().destroyLoader(loader.getId());
+	}
 
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
 
-    }
+	}
 
-    @OnClick(R.id.details_show_examples)
-    void onShowExamples() {
+	@OnClick(R.id.details_show_examples)
+	void onShowExamples() {
 
-        getLoaderManager().initLoader(2, null, this);
-    }
+		getLoaderManager().initLoader(2, null, this);
+	}
 }
