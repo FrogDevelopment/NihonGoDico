7949e6d397df3ced3312c7adb9dae794720a2569
Le Gall Benoît <benoit.legall@thanksys.com>
2020-01-27 09:59:55 +0100

    fix: preferences order & label

--- app/src/main/java/fr/frogdevelopment/nihongo/dico/ui/settings/SettingsFragment.java
+++ app/src/main/java/fr/frogdevelopment/nihongo/dico/ui/settings/SettingsFragment.java
@@ -22,29 +22,18 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         setSummary("settings_version", VERSION_NAME);
 
         SwitchPreference online = findPreference("settings_offline");
-        if (online != null && online.isChecked()) {
-//            online.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-//                @Override
-//                public boolean onPreferenceClick(Preference preference) {
-//                    return false;
-//                }
-//            });
-            online.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    setVisible("settings_entries", TRUE.equals(newValue));
-                    setVisible("settings_sentences", TRUE.equals(newValue));
-                    return true;
-                }
+        if (online != null) {
+            online.setOnPreferenceChangeListener((preference, newValue) -> {
+                handleDownloadsVisibility(TRUE.equals(newValue));
+                return true;
             });
 
-            setVisible("settings_entries", online.isChecked());
-            setVisible("settings_sentences", online.isChecked());
+            handleDownloadsVisibility(online.isChecked());
         }
     }
 
-    private void setVisible(String settings_entries, boolean visible) {
-        Preference preference = findPreference(settings_entries);
+    private void handleDownloadsVisibility(boolean visible) {
+        Preference preference = findPreference("settings_download_category");
         if (preference != null) {
             preference.setVisible(visible);
         }
