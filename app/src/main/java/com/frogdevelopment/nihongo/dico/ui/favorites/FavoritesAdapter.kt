30493d6a00ce4c50c53136ca1f93a89a22b2d8f6
Le Gall Benoit <legall.benoit@gmail.com>
2021-05-20 23:46:32 +0200

    feat(favorite): hide empty kana line

--- app/src/main/java/com/frogdevelopment/nihongo/dico/ui/favorites/FavoritesAdapter.kt
+++ app/src/main/java/com/frogdevelopment/nihongo/dico/ui/favorites/FavoritesAdapter.kt
@@ -2,12 +2,18 @@ package com.frogdevelopment.nihongo.dico.ui.favorites
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.frogdevelopment.nihongo.dico.data.entities.EntryDetails
 import com.frogdevelopment.nihongo.dico.databinding.SearchRowBinding
+import org.apache.commons.lang3.StringUtils
 
-class FavoritesAdapter(context: Context, private val listener: OnEntryClickListener, private val entries: List<EntryDetails>) : RecyclerView.Adapter<FavoritesAdapter.ViewHolder>() {
+class FavoritesAdapter(
+    context: Context,
+    private val listener: OnEntryClickListener,
+    private val entries: List<EntryDetails>
+) : RecyclerView.Adapter<FavoritesAdapter.ViewHolder>() {
 
     @FunctionalInterface
     interface OnEntryClickListener {
@@ -23,6 +29,11 @@ class FavoritesAdapter(context: Context, private val listener: OnEntryClickListe
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val entry = entries[position]
         holder.binding.header.text = entry.kanji
+        if (StringUtils.isBlank(entry.kanji)) {
+            holder.binding.header.visibility = View.GONE
+        } else {
+            holder.binding.header.visibility = View.VISIBLE
+        }
         holder.binding.subhead.text = entry.kana
         holder.binding.vocabulary.text = entry.gloss
         holder.itemView.setOnClickListener { listener.onEntryClick(entry) }
